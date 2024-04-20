ecaa7f1591b05bc7f4fc16f34ac619f9afc91ee7
irisus1 <22520210@gm.uit.edu.vn>
2024-04-20 17:39:42 +0700

    complete checkout page

--- resources/views/customer/checkout/index.blade.php
+++ resources/views/customer/checkout/index.blade.php
@@ -13,6 +13,24 @@
         <h2 class="font-bold text-[25px] pt-16 pl-24 pb-12">Billing Details</h2>
         <div class="flex">
             <div class="pl-24 w-[55%] ">
+                <div class="text-[20px] font-medium pb-4">Delivery Address *</div>
+                <div class="flex flex-row pb-10">
+
+                    <div class="flex items-center w-[45%] h-12 border-solid border-2 rounded">
+                        <label class="cursor-pointer pl-3">
+                            <input type="radio" id="same" name="address">
+                            <label for="same" class="text-[18px] pl-3 cursor-pointer">Same as shipping address</label>
+                        </label>
+                    </div>
+                    <div class="pl-14 w-[55%]">
+                        <div class="flex items-center h-12 border-solid border-2 rounded">
+                            <label class="cursor-pointer pl-3">
+                                <input type="radio" id="diff" name="address">
+                                <label for="diff" class="text-[18px] pl-3 cursor-pointer">Using a different billing address</label>
+                            </label>
+                        </div>
+                    </div>
+                </div>
                 <div class="flex flex-row pb-10">
                     <div class=" w-[60%]">
                         <div class="text-[20px] font-medium pb-4">First Name *</div>
@@ -62,10 +80,43 @@
             <div class="pl-28">
                 <div class="border-solid border-2 w-[230%] h-[50%] ">
                     <p class="font-bold text-[23px] pt-6 pl-6 pb-6">Order Summary</p>
-                    <div class="w-[90%] pl-14">
+                    <div class="w-[90%] pl-14 pb-8">
                         <hr>
                     </div>
+                    <div class="pl-6 ">
+                        <table class=" text-left">
+                            <tr>
+                                <th class="text-[#c9c9c9] w-[85%]">Item</th>
+                                <td class="w-fit text-right">8</td>
+                            </tr>
+                            <tr>
+                                <th class="text-[#c9c9c9] pt-7">Sub total</th>
+                                <td class="w-fit text-right pt-7">$607.60</td>
+                            </tr>
+                            <tr>
+                                <th class="text-[#c9c9c9] pt-7">Shipping</th>
+                                <td class="w-fit text-right pt-7">$00.00</td>
+                            </tr>
+                        </table>
+                    </div>
+                    <div class="w-[90%] pl-14 pt-10 pb-10">
+                        <hr>
+                    </div>
+                    <div class="pl-6 pb-10">
+                        <table class="text-left">
+                            <tr>
+                                <th class="text-[#c9c9c9]  w-[85%]">Total</th>
+                                <td class="w-fit text-right">$607.60</td>
+                            </tr>
+                        </table>
+                    </div>
+                    <form action="http://127.0.0.1:8000/checkout/confirmation">
+                        <div class="flex justify-center">
+                            <button type="submit" class="w-[80%] text-white bg-black h-16 text-[17px]">Continue to Payment</button>
+                        </div>
+                    </form>
                 </div>
+
             </div>
         </div>
     </div>
