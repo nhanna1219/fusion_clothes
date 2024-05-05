ff86a8bcfbe4cbd22bb7c69e695dd190ca0a4847
Hà Mai Anh <114716577+AnhHa03@users.noreply.github.com>
2024-05-05 08:06:35 +0700

    logic authentication

--- resources/views/components/checkbox.blade.php
+++ resources/views/components/checkbox.blade.php
@@ -1,1 +1,1 @@
-<input type="checkbox" {!! $attributes->merge(['class' => 'rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500']) !!}>
+<input type="checkbox" {!! $attributes->merge(['class' => 'focus:ring-0 focus:outline-none bg-gray-50 rounded-[4px] cursor-pointer text-black']) !!}>
--- routes/web.php
+++ routes/web.php
@@ -54,7 +54,5 @@ Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified']
     */
 
     // Dashboard - If this is for authenticated users, place it here
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+    Route::get('/dashboard', [HomeController::class, 'index'])->name('customer.home');
 });
