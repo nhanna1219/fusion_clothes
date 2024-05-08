13fca84b9a4d576b35ce6bbe751e9c897e8db914
Hà Mai Anh <114716577+AnhHa03@users.noreply.github.com>
2024-05-09 04:33:58 +0700

    the directory structure for the admin section

---
+++ resources/views/admin/order_management/order_processing.blade.php
@@ -0,0 +1,8 @@
+<x-app-layout>
+    <x-slot name="header"></x-slot>
+
+    <div>
+        <h1>Processing Orders</h1>  
+    </div>
+
+</x-app-layout>
---
+++ resources/views/admin/product_management/add_product.blade.php
@@ -0,0 +1,8 @@
+<x-app-layout>
+    <x-slot name="header"></x-slot>
+
+    <div>
+        <h1>Add New Product</h1>  
+    </div>
+
+</x-app-layout>
