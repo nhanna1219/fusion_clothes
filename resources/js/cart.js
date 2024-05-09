88f146233805d8638c1d0e99034e223619e50915
Kiet18hi <kiet13hi@gmail.com>
2024-05-09 13:27:41 +0700

    feat: create contact us page

--- resources/js/cart.js
+++ resources/js/cart.js
@@ -1,6 +1,33 @@
-let btnClear = document.querySelector("clear-btn");
-let inputs = document.querySelectorAll("input");
+document.addEventListener("DOMContentLoaded", function () {
+    const clearCartBtn = document.querySelector("#clear-cart");
+    function clearCart() {
+        // clear all products from cart (and local storage)
 
-btnClear.addEventListener("click", () => {
-    inputs.forEach((input) => (input.value = ""));
+        // retrieve list of products from LS
+        const lsContent = getLSContent();
+        // empty array in local storage
+        lsContent.splice(0, lsContent.length);
+        // update local storage
+        setLSContent(lsContent);
+        // display cart content again
+        displayProducts();
+    }
+    // Adding and subtract item quantity
+    const add = document.getElementById("increment-button");
+    const sub = document.getElementById("decrement-button");
+    const input = document.getElementById("quantity-input");
+    let a = 1;
+    if (add && sub) {
+        add.addEventListener("click", () => {
+            a++;
+            input.value = a;
+        });
+
+        sub.addEventListener("click", () => {
+            if (a > 1) {
+                a--;
+                input.value = a;
+            }
+        });
+    }
 });
