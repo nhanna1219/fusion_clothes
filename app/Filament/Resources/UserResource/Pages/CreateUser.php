08ce2a6ea61d8a6352587bbae9401b36b0526085
quark1412 <hvphuongdts@gmail.com>
2024-06-08 14:25:00 +0700

    fix: duplicate user

--- app/Filament/Resources/UserResource/Pages/CreateUser.php
+++ app/Filament/Resources/UserResource/Pages/CreateUser.php
@@ -3,13 +3,32 @@
 namespace App\Filament\Resources\UserResource\Pages;
 
 use App\Filament\Resources\UserResource;
+use App\Models\User;
 use Filament\Actions;
 use Filament\Notifications\Notification;
 use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Database\Eloquent\Model;
 
 class CreateUser extends CreateRecord
 {
     protected static string $resource = UserResource::class;
+
+    protected function handleRecordCreation(array $data): Model
+    {
+        $existingUser = User::where('email', $data['email'])
+            ->first();
+
+        if ($existingUser) {
+            Notification::make()
+                ->title('Error')
+                ->body('A user with the same email already exists.')
+                ->danger()
+                ->send();
+            $this->halt();
+        }
+
+        return parent::handleRecordCreation($data);
+    }
 
     protected function getCreatedNotification(): ?Notification
     {
